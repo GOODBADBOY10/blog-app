1f274f775d787619d458d2411658f7fcfac86a2e
ademola-adebisi <ademolaidris2002@gmail.com>
2024-07-20 17:12:08 +0100

    implement the routing function for 6 different pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,30 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Logout from './pages/Logout'
+import Projects from './pages/Projects'
 
 function App() {
 
   return (
-    <>
-      <div>App</div>
-    </>
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/project" element={<Projects/>} />
+
+
+        </Routes>
+
+        
+      </BrowserRouter>
   )
 }
 
