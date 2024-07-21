c42742b83b64932671bfb0be8512acca695f846e
ademola-adebisi <ademolaidris2002@gmail.com>
2024-07-21 21:23:23 +0100

    cretaed the login routes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import User from '../model/userModel.js';
 import { errorHandler } from '../utils/error.js';
 
@@ -23,6 +24,36 @@ export const signup = async (req, res, next) => {
     try {
         await newUser.save();
         res.status(201).json({ message: 'User registered successfully' });   
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+export const login = async (req, res, next) => {
+    const { email, password } = req.body;
+    // check if email and password are provided
+    if (!email || !password) {
+        return next(errorHandler(400, 'Please provide email and password'));
+    }
+
+    try {
+       const validUser = await User.findOne({ email }); 
+       if (!validUser) {
+            return next(errorHandler(401, 'User not found or password is incorrect'));
+       }
+       const isPasswordCorrect = bcrypt.compareSync(password, validUser.password);
+       if (!isPasswordCorrect) {
+            return next(errorHandler(401, 'Invalid email or password'));
+       }
+       const token = jwt.sign({ 
+        userId: validUser._id 
+        },
+        process.env.JWT_SECRET);
+        res.status(200).cookie('my-token', token, {
+            httpOnly: true,
+        }).json(validUser);
+
     } catch (error) {
         next(error);
     }
