bfcdec1e9cfa9b3589547bbf0fb854ab50598692
ademola-adebisi <ademolaidris2002@gmail.com>
2024-07-21 07:31:18 +0100

    hashed my password using bycrpt

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,15 +1,19 @@
+import bcrypt from 'bcryptjs';
 import User from '../model/userModel.js';
+
 
 export const signup = async (req, res) => {
     const { username, email, password } = req.body;
     if (!username || !email || !password || username === '' || email === '' || password === '') {
         return res.status(400).json({ error: 'All fields are required' });
     }
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
     // Add user to the database
     const newUser = new User({
         username,
         email,
-        password
+        password: hashedPassword,
     });
     try {
         await newUser.save();
