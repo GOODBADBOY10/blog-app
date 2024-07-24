de7dcc60471ae4636e61d0ca7dbc2c30ee1f7a9e
ademola-adebisi <ademolaidris2002@gmail.com>
2024-07-24 20:06:30 +0100

    privatize the dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
 import Header from './components/Header'
 import Footers from './components/Footers'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
 
@@ -19,7 +20,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="dashboard" element={<Dashboard />} />
+          </Route>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/project" element={<Projects/>} />
