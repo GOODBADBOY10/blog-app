984f638070fac3fc627ce74a91a05fc0ecf8500b
ademola-adebisi <ademolaidris2002@gmail.com>
2024-07-25 08:31:49 +0100

    cmpleted the update profile functionality

--- frontend/src/components/DashProfile.jsx
+++ frontend/src/components/DashProfile.jsx
@@ -5,6 +5,8 @@ import {getStorage, uploadBytesResumable, ref, getDownloadURL} from 'firebase/st
 import {app} from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { updateFailure, updateStart, updateSuccess } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 function DashProfile() {
     const {currentUser} = useSelector((state) => state.user)
@@ -12,6 +14,10 @@ function DashProfile() {
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadingProgress, setImageFileUploadingProgress] = useState(0);
     const [imageFileUploadError, setImageFileUploadError] = useState(null);
+    const [formData, setFormData] = useState({});
+    const [imageFileUploadingStatus, setImageFileUploadingStatus] = useState(false);
+    const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+    const dispatch = useDispatch();
     console.log(imageFileUploadingProgress, imageFileUploadError);
     const filePickerRef = useRef();
 
@@ -44,6 +50,7 @@ function DashProfile() {
 //     }
 //   }
 // }
+        setImageFileUploadingStatus(true);
         setImageFileUploadError(null); 
         const storage = getStorage(app);
         const fileName = new Date().getTime() + imageFile.name;
@@ -60,18 +67,56 @@ function DashProfile() {
           setImageFileUploadingProgress(null);
           setImageFileUrl(null);
           setImageFile(null);
+          setImageFileUploadingStatus(false);
         },
         () => {
             getDownloadURL(uploadImageTask.snapshot.ref).then((downloadURL) => {
                 setImageFileUrl(downloadURL);
+                setFormData({...formData, profilePicture: downloadURL });
+                setImageFileUploadingStatus(false);
         });  
     })
+}
+
+const handleChange = (e) => {
+  setFormData({...formData, [e.target.id]: e.target.value })
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  // update user profile in firestore
+  if(Object.keys(formData).length === 0) {
+    return;
+  }
+  if(imageFileUploadingStatus) {
+    return;
+  }
+  try {
+    dispatch(updateStart());
+    const response = await fetch(`/api/user/update/${currentUser._id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    } )
+    const data = await response.json();
+    if(!response.ok) {
+      dispatch(updateFailure(data.message));
+      return;
+    } else{
+      dispatch(updateSuccess(data));
+      setUpdateUserSuccess('user profile updated successfully');
+    }
+  } catch (error) {
+    dispatch(updateFailure(error.message));
+  }
 }
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 
       className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form 
+      className='flex flex-col gap-4'
+      onSubmit={handleSubmit}>
         <input 
         type='file' 
         accept='image/=' 
@@ -123,17 +168,20 @@ function DashProfile() {
         id='username' 
         placeholder='username' 
         defaultValue={currentUser.username}
+        onChange={handleChange}
         />
         <TextInput 
         type='email' 
         id='email' 
         placeholder='email' 
         defaultValue={currentUser.email} 
+        onChange={handleChange}
         />
         <TextInput 
         type='password' 
         id='password' 
-        placeholder='password' 
+        placeholder='password'
+        onChange={handleChange} 
         />
         <Button type='submit' gradientDuoTone='purpleToBlue' outline>Update</Button>
       </form>
@@ -141,6 +189,11 @@ function DashProfile() {
         <span className='cursor-pointer'>Delete Account</span>
         <span className='cursor-pointer'>Logout</span>
       </div>
+      {updateUserSuccess && (
+        <Alert color='success' className='mt-5'>
+          {updateUserSuccess}
+        </Alert>
+      )}
     </div>
   )
 }
